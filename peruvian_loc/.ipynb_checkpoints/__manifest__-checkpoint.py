4130ab191355f1c7fa7dac9233aaab332cc08a2d
Jose Arguedas <jose.arguedas@neora.com.pe>
2019-06-19 15:53:51 +0000

    peruvian_loc

---
+++ peruvian_loc/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Perú Localizaciones",
+
+    'summary': """
+        Odoo Perú Localizaciones""",
+
+    'description': """
+        Odoo Perú Localizaciones
+    """,
+
+    'author': "Neora",
+    'website': "http://www.neora.com.pe",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',        
+        'views/loc_reports.xml',
+        'views/registro_compras_views.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ peruvian_loc/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import loc_reports
+from . import registro_compras
